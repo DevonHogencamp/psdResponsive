c6449a7912a7047cb3ca525b395a06481dc207e9
Devon Hogencamp <devon.hogencamp@west-mec.org>
2016-11-03 15:03:38 -0700

    Added the slider functionality

--- menu.js
+++ menu.js
@@ -1,5 +1,35 @@
-$(document).ready(function () {
-    $("#burgerNav").on("click", function(){
-        $("header nav ul").toggleClass("open");
-    });
-});
+window.onload = function () {
+    var burger = document.getElementById('burgerNav');
+    var menu = document.getElementById('menu');
+    var height = 0;
+    var animation;
+
+    burger.onclick = function () {
+        function addHeight() {
+            if (height == 222) {
+            clearInterval(animation);
+            }
+            else {
+                height += 2;
+                menu.style.height = height + 'px';
+            }
+        }
+
+        function subHeight() {
+            if (height === 0) {
+            clearInterval(animation);
+            }
+            else {
+                height -= 2;
+                menu.style.height = height + 'px';
+            }
+        }
+
+        if (height === 0) {
+            animation = setInterval(addHeight, 1);
+        }
+        else {
+            animation = setInterval(subHeight, 1);
+        }
+    };
+};
